5fa682261a0695e0db48666784752c2be616a80f
anklesh-chauhan <your-email@example.com>
2025-05-07 22:48:31 +0530

    12

--- app/Policies/AccountMasterPolicy.php
+++ app/Policies/AccountMasterPolicy.php
@@ -5,103 +5,104 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\AccountMaster;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\TenantUser;
 
 class AccountMasterPolicy
 {
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
+     * Determine whether the User|TenantUser can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User|TenantUser $user): bool
     {
         return $user->can('view_any_account::master');
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the User|TenantUser can view the model.
      */
-    public function view(User $user, AccountMaster $accountMaster): bool
+    public function view(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('view_account::master');
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the User|TenantUser can create models.
      */
-    public function create(User $user): bool
+    public function create(User|TenantUser $user): bool
     {
         return $user->can('create_account::master');
     }
 
     /**
-     * Determine whether the user can update the model.
+     * Determine whether the User|TenantUser can update the model.
      */
-    public function update(User $user, AccountMaster $accountMaster): bool
+    public function update(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('update_account::master');
     }
 
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the User|TenantUser can delete the model.
      */
-    public function delete(User $user, AccountMaster $accountMaster): bool
+    public function delete(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('delete_account::master');
     }
 
     /**
-     * Determine whether the user can bulk delete.
+     * Determine whether the User|TenantUser can bulk delete.
      */
-    public function deleteAny(User $user): bool
+    public function deleteAny(User|TenantUser $user): bool
     {
         return $user->can('delete_any_account::master');
     }
 
     /**
-     * Determine whether the user can permanently delete.
+     * Determine whether the User|TenantUser can permanently delete.
      */
-    public function forceDelete(User $user, AccountMaster $accountMaster): bool
+    public function forceDelete(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('force_delete_account::master');
     }
 
     /**
-     * Determine whether the user can permanently bulk delete.
+     * Determine whether the User|TenantUser can permanently bulk delete.
      */
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(User|TenantUser $user): bool
     {
         return $user->can('force_delete_any_account::master');
     }
 
     /**
-     * Determine whether the user can restore.
+     * Determine whether the User|TenantUser can restore.
      */
-    public function restore(User $user, AccountMaster $accountMaster): bool
+    public function restore(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('restore_account::master');
     }
 
     /**
-     * Determine whether the user can bulk restore.
+     * Determine whether the User|TenantUser can bulk restore.
      */
-    public function restoreAny(User $user): bool
+    public function restoreAny(User|TenantUser $user): bool
     {
         return $user->can('restore_any_account::master');
     }
 
     /**
-     * Determine whether the user can replicate.
+     * Determine whether the User|TenantUser can replicate.
      */
-    public function replicate(User $user, AccountMaster $accountMaster): bool
+    public function replicate(User|TenantUser $user, AccountMaster $accountMaster): bool
     {
         return $user->can('replicate_account::master');
     }
 
     /**
-     * Determine whether the user can reorder.
+     * Determine whether the User|TenantUser can reorder.
      */
-    public function reorder(User $user): bool
+    public function reorder(User|TenantUser $user): bool
     {
         return $user->can('reorder_account::master');
     }
