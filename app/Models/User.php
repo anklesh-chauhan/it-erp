31a7c618c10afd1a64c3dfac40ba5928c6e64a60
anklesh-chauhan <your-email@example.com>
2025-05-10 22:52:19 +0530

    121

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,10 +13,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
-    protected $connection = 'tenant';
-
-    protected $guard_name = 'tenant';
-
     /**
      * The attributes that are mass assignable.
      *
