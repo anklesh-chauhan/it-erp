cfe919476414d190a8eadc0e17807a79ffaf5817
anklesh-chauhan <your-email@example.com>
2025-05-10 16:49:41 +0530

    123

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-use App\Models\Tenant;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    protected $connection = 'tenant';
+
+    protected $guard_name = 'tenant';
 
     /**
      * The attributes that are mass assignable.
